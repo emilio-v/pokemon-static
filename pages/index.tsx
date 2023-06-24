9e3d83ea2958c929e020feb04fe97dbc7303ec19
Emilio Velasco <emivelasco1998@outlook.es>
2023-06-23 22:39:25 -0600

    integrate NextUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@nextui-org/react";
+
 const HomePage = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <Button color="gradient">Hello World</Button>
     </>
   );
 };
