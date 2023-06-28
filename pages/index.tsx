1dbb5d395410736ba13fb12901b8c74734f7f7f1
Emilio Velasco <emivelasco1998@outlook.es>
2023-06-28 10:47:07 -0600

    display pokemon name and id

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,44 @@
+import { pokeApi } from "@/api";
 import { Layout } from "@/components/layouts";
+import { PokemonListResponse, SmallPokemon } from "@/interfaces";
 import { Button } from "@nextui-org/react";
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 
-const HomePage = () => {
+interface HomePageProps {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<HomePageProps> = ({ pokemons }) => {
+  console.log(pokemons);
   return (
     <Layout title="Pokemon List">
-      <Button color="gradient">Hello World</Button>
+      <ul>
+        {pokemons.map(({ name, id }) => (
+          <li key={id}>
+            {id} - {name}
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
+
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, idx) => ({
+    ...pokemon,
+    id: idx + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      idx + 1
+    }.svg`,
+  }));
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
 };
 
 export default HomePage;
