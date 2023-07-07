b0d93a069b5b51d19d430b3a5d0f1edf883ddd1e
Emilio Velasco <emivelasco1998@outlook.es>
2023-07-06 23:01:16 -0600

    open graph meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,6 +7,8 @@ type LayoutProps = {
   title?: string;
 };
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout: FC<LayoutProps> = ({
   children,
   title = "Pokemon App",
@@ -21,6 +23,13 @@ export const Layout: FC<LayoutProps> = ({
           content={`Information about pokemon ${title}`}
         />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`Information about ${title}`} />
+        <meta
+          property="og:description"
+          content={`This is the page about ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/images/banner.png`} />
       </Head>
 
       <Navbar />
